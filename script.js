53394a3bdbaa3624b9d475333c7157539f2bdd8a
Rajkirantagde <121598976+rajutagde@users.noreply.github.com>
2023-04-23 19:31:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+//your JS code here. If required. const levelElement = document.getElementById('level');
+let level = 0;
+
+while (levelElement.parentNode) {
+  level++;
+  levelElement = levelElement.parentNode;
+}
+
+alert(`The level of the element is: ${level}`);
+
